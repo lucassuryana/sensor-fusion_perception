c81a6926fb541dda97b7ac018f64d4d272dcbe32
lsuryana <lucaselbert@gmail.com>
2025-01-28 17:46:35 +0100

    Finish TODO 1

--- project/student/filter.py
+++ project/student/filter.py
@@ -16,77 +16,111 @@ import numpy as np
 # add project directory to python path to enable relative imports
 import os
 import sys
+
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-import misc.params as params 
+import misc.params as params
+
 
 class Filter:
     '''Kalman filter class'''
+
     def __init__(self):
         pass
 
     def F(self):
         ############
-        # TODO Step 1: implement and return system matrix F
+        # Step 1: implement and return system matrix F
         ############
+        dt = params.dt
+        F = np.eye(params.dim_state)  # Identity matrix for state transition
+        F[0, 3] = dt  # Update position x with velocity x
+        F[1, 4] = dt  # Update position y with velocity y
+        F[2, 5] = dt  # Update position z with velocity z
+        return F
 
-        return 0
-        
         ############
         # END student code
-        ############ 
+        ############
 
     def Q(self):
         ############
-        # TODO Step 1: implement and return process noise covariance Q
+        # Step 1: implement and return process noise covariance Q
         ############
+        dt = params.dt
+        q = params.q
+        Q = np.zeros((params.dim_state, params.dim_state))
+        Q[0, 0] = (dt ** 3) / 3 * q  # Variance for position x
+        Q[1, 1] = (dt ** 3) / 3 * q  # Variance for position y
+        Q[2, 2] = (dt ** 3) / 3 * q  # Variance for position z
+        Q[0, 3] = (dt ** 2) / 2 * q  # Covariance between position x and velocity x
+        Q[1, 4] = (dt ** 2) / 2 * q  # Covariance between position y and velocity y
+        Q[2, 5] = (dt ** 2) / 2 * q  # Covariance between position z and velocity z
+        Q[3, 0] = (dt ** 2) / 2 * q
+        Q[4, 1] = (dt ** 2) / 2 * q
+        Q[5, 2] = (dt ** 2) / 2 * q
+        Q[3, 3] = dt * q  # Variance for velocity x
+        Q[4, 4] = dt * q  # Variance for velocity y
+        Q[5, 5] = dt * q  # Variance for velocity z
+        return Q
 
-        return 0
-        
         ############
         # END student code
-        ############ 
+        ############
 
     def predict(self, track):
         ############
-        # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
+        # Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
         ############
+        F = self.F()
+        Q = self.Q()
+        x_pred = F @ track.x  # Predict state
+        P_pred = F @ track.P @ F.T + Q  # Predict covariance
+        track.set_x(x_pred)
+        track.set_P(P_pred)
 
-        pass
-        
         ############
         # END student code
-        ############ 
+        ############
 
     def update(self, track, meas):
         ############
-        # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
+        # Step 1: update state x and covariance P with associated measurement, save x and P in track
         ############
-        
+        H = meas.sensor.get_H(track.x)  # Measurement matrix
+        gamma = self.gamma(track, meas)  # Residual
+        S = self.S(track, meas, H)  # Covariance of residual
+        K = track.P @ H.T @ np.linalg.inv(S)  # Kalman gain
+        x_updated = track.x + K @ gamma  # Update state
+        P_updated = (np.eye(params.dim_state) - K @ H) @ track.P  # Update covariance
+        track.set_x(x_updated)
+        track.set_P(P_updated)
+
         ############
         # END student code
-        ############ 
+        ############
         track.update_attributes(meas)
-    
+
     def gamma(self, track, meas):
         ############
-        # TODO Step 1: calculate and return residual gamma
+        # Step 1: calculate and return residual gamma
         ############
+        H = meas.sensor.get_H(track.x)  # Measurement matrix
+        gamma = meas.z - H @ track.x  # Residual
+        return gamma
 
-        return 0
-        
         ############
         # END student code
-        ############ 
+        ############
 
     def S(self, track, meas, H):
         ############
-        # TODO Step 1: calculate and return covariance of residual S
+        # Step 1: calculate and return covariance of residual S
         ############
+        S = H @ track.P @ H.T + meas.R  # Covariance of residual
+        return S
 
-        return 0
-        
         ############
         # END student code
         ############ 
